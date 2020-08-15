a484a512319f3ffe9b4697815816e36d47f12a04
lym <1093567783@qq.com>
2020-08-15 09:25:10 +0800

    lym使用分支，ljx修改项目

--- src/test/lym.java
+++ src/test/lym.java
@@ -3,6 +3,7 @@ public class lym {
     public void add(){
         System.out.println("lym");
         System.out.println("ljx导入git项目");
+        System.out.println("lym使用分支后，ljx修改主分支");
     }
 
 }
